830bf29581f699870933a1521f66dc0ab6d95a29
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-10-05 21:00:58 +0100

    Add MySQL connection and integrate with main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/bohexists/task-manager-svc/config"
+	"github.com/bohexists/task-manager-svc/internal/adapters/db"
 	"github.com/bohexists/task-manager-svc/internal/adapters/grpc"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+	db.ConnectToDB(cfg)
 	grpc.StartGRPCServer()
 }
